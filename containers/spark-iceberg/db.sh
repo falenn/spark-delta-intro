36a0f8ef8b31ea8e6089a5eab9dc1e493400b836
falenn <dev@imwiz.com>
2023-09-20 17:19:37 +0000

    build script

---
+++ containers/spark-iceberg/db.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Docker buildcycle script
+# Build and automate cleanup - good to use on a dev box where this is the
+# only project you are workingo on.
+
+REPO_AND_IMAGE='falenn/iceberg-python:1.0.0'
+CONTAINER_NAME='iceberg-python'
+
+
+#stop any running containers
+sudo docker ps | grep $CONTAINER_NAME | awk '{print $1}' | xargs docker stop
+
+# remove any existing stopped containers
+sudo docker ps -a | grep $CONTAINER_NAME | awk '{print $1}' | xargs docker rm
+
+# remove built image for rebuild
+# ßdocker rmi $(docker images | grep -v $REPO_AND_IMAGE | awk {'print $3'})
+
+# remove any images that are left around
+# docker rmi $(docker images -f dangling=true -q)
+
+# build the image, removing intermediate layers, deleting cache
+# docker build --rm --no-cache -t "$REPO_AND_IMAGE" .
+sudo docker build \
+    --rm \
+    -t "$REPO_AND_IMAGE" .
+
+if [ $? -eq 0 ]; then
+  # run the newly built image
+  #docker run --name $CONTAINER_NAME -p 25565:25565 -l $CONTAINER_NAME $REPO_AND_IMAGE
+
+  # My kernel information
+  # Linux 64cd1417ea3a 4.4.27-boot2docker #1 SMP Tue Oct 25 19:51:49 UTC 2016 x86_64 Linux
+
+  # run in inteactive for debugging / development
+  sudo docker run \
+        --name $CONTAINER_NAME \
+        -l $CONTAINER_NAME \
+        -it \
+        --entrypoint="/bin/bash" \
+        $REPO_AND_IMAGE
+
+  # attach to the new container
+  sudo docker attach -it $CONTAINER_NAME
+
+  # follow stdout
+  #docker logs -f $CONTAINER_NAME
+
+else
+  echo "Failed to build"
+fi
